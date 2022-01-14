665c74cdcd11307e912305a2903519f699d0aa0b
ميخائيل <88463490+1p9@users.noreply.github.com>
2022-01-14 14:58:03 -0800

    Update duck-adress.py

--- duck-adress.py
+++ duck-adress.py
@@ -3,31 +3,27 @@ import requests
 import os
 from os import system
 
-system("title " + "By Lynch - Duck IP Adress Information")
+system("title " + "Programmed By Lynch - IP Adress Information")
 import colorama
 from colorama import Fore
 
 colorama.init(autoreset=True)
 from time import sleep
 
-sleep(0.5)
 logo = """
-
   _                     _     
  | |   _   _ _ __   ___| |__  
  | |  | | | | '_ \ / __| '_ \ 
  | |__| |_| | | | | (__| | | |
  |_____\__, |_| |_|\___|_| |_|
        |___/                                    
-
 """
-sleep(0.3)
 
 print(Fore.CYAN+logo)
-title = ("By Lynch, Telegram: @overexcited")
-print(Fore.RED+title)
-sleep(0.6)
+title = ("Made w Love By Lynch, [i]: @l7up")
+print(Fore.RED+title) 
 ip2 = input(f"[{Fore.GREEN}?{Fore.RESET}] Ducks IP Adress: ")
+
 response = requests.post("http://ip-api.com/batch", json=[
     {
         "query": ip2
@@ -36,9 +32,9 @@ response = requests.post("http://ip-api.com/batch", json=[
 sleep(0)
 for ip_info in response:
     for k, v in ip_info.items():
-        print("+-------------------+")
 
 
-        print(f"\n[{Fore.GREEN}i{Fore.RESET}] " + k, v)
+        print("+" + "-" * 10 + "Successfully" + "-" * 10 + "+")
+        print(f"\n[{Fore.CYAN}i{Fore.RESET}] " + k, v)
     print("\n")
-input(f"[{Fore.GREEN}+{Fore.RESET}] Made w Love, Socials: [I] @entrysquad - [T] @overexcited")
+input(f"[{Fore.MAGENTA}${Fore.RESET}] Press ENTER To Exit...") 
